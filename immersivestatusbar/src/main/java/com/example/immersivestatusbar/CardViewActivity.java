702304fb21ca4bec41d9fd30a276879e2a7d5474
wenjie.shen <wenjie.shen@baidao.com>
2016-07-25 18:06:40 +0800

    增加了ToolBar相关的用例

---
+++ immersivestatusbar/src/main/java/com/example/immersivestatusbar/CardViewActivity.java
@@ -0,0 +1,13 @@
+package com.example.immersivestatusbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CardViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_view);
+    }
+}
---
+++ immersivestatusbar/src/main/java/com/example/immersivestatusbar/CollapsingToolBarActivity.java
@@ -0,0 +1,14 @@
+package com.example.immersivestatusbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CollapsingToolBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collapsing_tool_bar);
+    }
+}
